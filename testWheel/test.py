17c1bca203f89e291f30f7f9ab490f2915413672
tigerfsh <mrsmish@yahoo.com>
2022-11-11 16:34:20 +0800

    first commit

---
+++ testWheel/test.py
@@ -0,0 +1,10 @@
+from testWheel.__init__ import *
+
+def func_test():
+    print("Successfully Imported test.py file")
+
+def print_name(name):
+    print(f'Hello {name}')
+
+def print_test_age(age):
+    print_age(age)
